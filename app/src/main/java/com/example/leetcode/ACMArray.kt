c509a8e0c2f2c78da8588e9c6bd1603d07386f93
wxxewx <419587118@qq.com>
2019-05-23 21:56:23 +0800

    只出现一次的数字

--- app/src/main/java/com/example/leetcode/ACMArray.kt
+++ app/src/main/java/com/example/leetcode/ACMArray.kt
@@ -104,11 +104,72 @@ class ACMArray {
         for (i in 1..k) {
             for (j in 0 until nums.size) {
                 var num = 0
-                num = nums[nums.size-1]
-                nums[nums.size-1] = nums[j]
+                num = nums[nums.size - 1]
+                nums[nums.size - 1] = nums[j]
                 nums[j] = num
 
             }
         }
+    }
+
+
+    /**
+     * 给定一个整数数组，判断是否存在重复元素。
+
+    如果任何值在数组中出现至少两次，函数返回 true。如果数组中每个元素都不相同，则返回 false。
+
+    示例 1:
+
+    输入: [1,2,3,1]
+    输出: true
+    示例 2:
+
+    输入: [1,2,3,4]
+    输出: false
+    示例 3:
+
+    输入: [1,1,1,3,3,4,3,2,4,2]
+    输出: true
+     */
+    fun containsDuplicate(nums: IntArray): Boolean {
+        for (i in 0 until nums.size) {
+            val num = nums[i]
+            for (j in (i + 1) until nums.size) {
+                val num2 = nums[j]
+                if (num == num2) {
+                    return true
+                }
+            }
+        }
+        return false
+    }
+
+
+    /**
+     *
+     * 给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+
+    说明：
+
+    你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
+
+    示例 1:
+
+    输入: [2,2,1]
+    输出: 1
+    示例 2:
+
+    输入: [4,1,2,1,2]
+    输出: 4
+     */
+
+    fun singleNumber(nums: IntArray): Int {
+
+        var single = 0
+
+        for (i in 0 until nums.size) {
+            single = single xor nums[i]
+        }
+        return single
     }
 }
--- app/src/test/java/com/example/leetcode/ACMArrayTest.kt
+++ app/src/test/java/com/example/leetcode/ACMArrayTest.kt
@@ -10,31 +10,31 @@ public class ACMArrayTest {
     fun deleteDuplicatesFromSortedArrays_test() {
         val intArrayOf = intArrayOf(1, 1, 2, 2, 3, 4)
         assert(acmArray.deleteDuplicatesFromSortedArrays(intArrayOf) == 4)
-        assert(intArrayOf[0]==1)
-        assert(intArrayOf[1]==2)
-        assert(intArrayOf[2]==3)
-        assert(intArrayOf[3]==4)
+        assert(intArrayOf[0] == 1)
+        assert(intArrayOf[1] == 2)
+        assert(intArrayOf[2] == 3)
+        assert(intArrayOf[3] == 4)
 
         val intArrayOf2 = intArrayOf(1, 1, 2, 2, 3, 4, 4)
         assert(acmArray.deleteDuplicatesFromSortedArrays(intArrayOf2) == 4)
-        assert(intArrayOf2[0]==1)
-        assert(intArrayOf2[1]==2)
-        assert(intArrayOf2[2]==3)
-        assert(intArrayOf2[3]==4)
+        assert(intArrayOf2[0] == 1)
+        assert(intArrayOf2[1] == 2)
+        assert(intArrayOf2[2] == 3)
+        assert(intArrayOf2[3] == 4)
 
 
         val intArrayOf3 = intArrayOf(0, 1, 1, 2, 2, 3, 4)
         assert(acmArray.deleteDuplicatesFromSortedArrays(intArrayOf3) == 5)
-        assert(intArrayOf3[0]==0)
-        assert(intArrayOf3[1]==1)
-        assert(intArrayOf3[2]==2)
-        assert(intArrayOf3[3]==3)
-        assert(intArrayOf3[4]==4)
+        assert(intArrayOf3[0] == 0)
+        assert(intArrayOf3[1] == 1)
+        assert(intArrayOf3[2] == 2)
+        assert(intArrayOf3[3] == 3)
+        assert(intArrayOf3[4] == 4)
 
 
         val intArrayOf4 = intArrayOf(0)
         assert(acmArray.deleteDuplicatesFromSortedArrays(intArrayOf4) == 1)
-        assert(intArrayOf4[0]==0)
+        assert(intArrayOf4[0] == 0)
 
 
         val intArrayOf5 = intArrayOf()
@@ -44,13 +44,14 @@ public class ACMArrayTest {
 
 
     @Test
-    fun  maxProfit_test(){
-        var array1= intArrayOf(7,1,5,3,6,4)
-        var array2= intArrayOf(1,2,3,4,5)
-        var array3= intArrayOf(7,6,4,3,1)
+    fun maxProfit_test() {
+        var array1 = intArrayOf(7, 1, 5, 3, 6, 4)
+        var array2 = intArrayOf(1, 2, 3, 4, 5)
+        var array3 = intArrayOf(7, 6, 4, 3, 1)
 
-        assert(acmArray.maxProfit(array1)==7)
-        assert(acmArray.maxProfit(array2)==4)
-        assert(acmArray.maxProfit(array3)==0)
+        assert(acmArray.maxProfit(array1) == 7)
+        assert(acmArray.maxProfit(array2) == 4)
+        assert(acmArray.maxProfit(array3) == 0)
     }
+
 }
