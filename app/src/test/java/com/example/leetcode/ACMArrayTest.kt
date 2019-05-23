c641f3b1087c96d0cea409a19d4d497b6113a476
wxxewx <419587118@qq.com>
2019-05-23 19:38:50 +0800

    deleteDuplicatesFromSortedArrays 从排序数组中删除重复项

---
+++ app/src/test/java/com/example/leetcode/ACMArrayTest.kt
@@ -0,0 +1,44 @@
+package com.example.leetcode
+
+import org.junit.Test
+
+public class ACMArrayTest {
+
+    val acmArray = ACMArray()
+
+    @Test
+    fun deleteDuplicatesFromSortedArrays_test() {
+        val intArrayOf = intArrayOf(1, 1, 2, 2, 3, 4)
+        assert(acmArray.deleteDuplicatesFromSortedArrays(intArrayOf) == 4)
+        assert(intArrayOf[0]==1)
+        assert(intArrayOf[1]==2)
+        assert(intArrayOf[2]==3)
+        assert(intArrayOf[3]==4)
+
+        val intArrayOf2 = intArrayOf(1, 1, 2, 2, 3, 4, 4)
+        assert(acmArray.deleteDuplicatesFromSortedArrays(intArrayOf2) == 4)
+        assert(intArrayOf2[0]==1)
+        assert(intArrayOf2[1]==2)
+        assert(intArrayOf2[2]==3)
+        assert(intArrayOf2[3]==4)
+
+
+        val intArrayOf3 = intArrayOf(0, 1, 1, 2, 2, 3, 4)
+        assert(acmArray.deleteDuplicatesFromSortedArrays(intArrayOf3) == 5)
+        assert(intArrayOf3[0]==0)
+        assert(intArrayOf3[1]==1)
+        assert(intArrayOf3[2]==2)
+        assert(intArrayOf3[3]==3)
+        assert(intArrayOf3[4]==4)
+
+
+        val intArrayOf4 = intArrayOf(0)
+        assert(acmArray.deleteDuplicatesFromSortedArrays(intArrayOf4) == 1)
+        assert(intArrayOf4[0]==0)
+
+
+        val intArrayOf5 = intArrayOf()
+        assert(acmArray.deleteDuplicatesFromSortedArrays(intArrayOf5) == 0)
+
+    }
+}
