99046d935208d2d0790fa5c30c20d987d26ad7fa
wxxewx <419587118@qq.com>
2019-05-24 18:53:16 +0800

    移动零

--- app/src/main/java/com/example/leetcode/ACMArray.kt
+++ app/src/main/java/com/example/leetcode/ACMArray.kt
@@ -3,7 +3,6 @@ package com.example.leetcode
 import java.nio.file.Files.size
 
 
-
 class ACMArray {
 
 
@@ -297,5 +296,36 @@ class ACMArray {
             digits[size - 1] = num + 1
         }
         return digits
+    }
+
+
+    /**
+     * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+
+    示例:
+
+    输入: [0,1,0,3,12]
+    输出: [1,3,12,0,0]
+    说明:
+
+    必须在原数组上操作，不能拷贝额外的数组。
+    尽量减少操作次数。
+     */
+
+    fun moveZeroes(nums: IntArray): Unit {
+        if (nums == null || nums.size === 0) {
+            return
+        }
+        //记录非o元素开始位置
+        var k = 0
+        for (i in 0 until nums.size) {
+            if (nums[i] !== 0) {
+                nums[k++] = nums[i]
+            }
+        }
+        while (k < nums.size) {
+            nums[k] = 0
+            k++
+        }
     }
 }
