c4fde96ccbd6660ea2152baea71b839b1e4c9451
wxxewx <419587118@qq.com>
2019-05-27 21:34:07 +0800

    反转int数字

--- app/src/main/java/com/example/leetcode/ACMString.kt
+++ app/src/main/java/com/example/leetcode/ACMString.kt
@@ -24,11 +24,56 @@ class ACMString {
      */
     fun reverseString(s: CharArray): Unit {
         var char: Char = 'a'
-        for (i in 0 until s.size / 2 ) {
+        for (i in 0 until s.size / 2) {
             val c = s[i]
             char = s[s.size - 1 - i]
-            s[s.size - 1 - i]=c
-            s[i]=char
+            s[s.size - 1 - i] = c
+            s[i] = char
         }
+    }
+
+    /**
+     * 给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
+
+    示例 1:
+
+    输入: 123
+    输出: 321
+    示例 2:
+
+    输入: -123
+    输出: -321
+    示例 3:
+
+    输入: 120
+    输出: 21
+     */
+    fun reverse(x: Int): Int {
+        var temp = x
+        var reverse = 0
+        while (true) {
+            val i = temp % 10
+            temp /= 10
+            reverse = reverse * 10 + i
+
+            if (reverse > Int.MAX_VALUE / 10) {
+                reverse = 0
+                break
+            }
+            if (reverse < Int.MIN_VALUE / 10) {
+                reverse = 0
+                break
+            }
+
+            if (temp == temp % 10) {
+                if (temp != 0) {
+                    reverse = reverse * 10 + temp
+                }
+                break
+            }
+        }
+        return reverse
+
+
     }
 }
---
+++ app/src/test/java/com/example/leetcode/ACMStringTest.kt
@@ -0,0 +1,15 @@
+package com.example.leetcode
+
+import org.junit.Test
+
+public class ACMStringTest {
+
+    val acmString = ACMString()
+
+
+
+    @Test
+    fun reverse_test(){
+        acmString.reverse(1534236469)
+    }
+}
